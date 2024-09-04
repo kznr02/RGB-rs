99aaa675745e2f2d5320c31793fbb49f1d0c15d0
Mizuiro <clyde0203hy@outlook.com>
2024-09-05 03:50:50 +0800

    feat: Add basic cmd branch

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
+use clap::{arg, command, Parser, Subcommand, ValueEnum};
+
+#[derive(Parser, Debug)]
+#[command(version = "1.0")]
+struct Cli {
+    #[command(subcommand)]
+    commands: SubCommands,
+}
+
+#[derive(Debug, Subcommand)]
+enum SubCommands {
+    Detect,
+    Get {
+        #[arg(short)]
+        device: String,
+
+        prop: ValType,
+    },
+    Set {
+        #[arg(short)]
+        device: String
+    }
+}
+
+#[derive(Debug, ValueEnum, Clone)]
+enum ValType {
+    Brightness,
+    Color,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match args.commands {
+        SubCommands::Detect => {
+            println!("You are in detect!");
+        },
+        SubCommands::Get{device, prop} => {
+            println!("device name: {:?}", device)
+        },
+        SubCommands::Set{device} => {
+            println!("device name: {:?}", device)
+        }
+    }
 }
